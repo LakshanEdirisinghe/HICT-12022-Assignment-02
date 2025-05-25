6687370e2dc6a78122b81e756952f91853713d1d
git config --global LakshanEdirisinghe <Ayomall30@gmail.com>
2025-05-25 16:51:52 +0530

    HICT 12022-Assignment 02

---
+++ dog.java
@@ -0,0 +1,50 @@
+class dog{
+	
+	public static void main(String cat[]){
+		
+		Car carObj = new Car();
+		double resultcar = carObj.calculateRental(8);
+		
+		System.out.println("Rs: "+resultcar);
+		
+		Bike bikeObj = new Bike();
+		double resultbike = bikeObj.calculateRental(5);
+		
+		System.out.println("Rs: "+resultbike);
+		
+		
+		}
+	
+	}
+	
+class Vehicle{
+
+	double calculateRental(int days){
+		
+		return 0;
+		
+		}
+	
+	
+}
+
+class Car extends Vehicle{ 
+	
+		double calculateRental(int days){
+		
+			return days * 50.0;
+		
+		}
+	
+	
+	}
+class Bike extends Vehicle{ 
+	
+		double calculateRental(int days){
+		
+			return days * 20.0;
+		
+		}
+	
+	
+	}
